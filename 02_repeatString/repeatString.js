893907d80eced0cd92c838af7457401b5af70058
Bailey <baileywyoung@gmail.com>
2023-02-07 20:06:02 -0800

    Passed all tests for problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(n,k) {
+    let a = '';
+    (k < 0) ? a = 'ERROR' : a = '';
+    for (let i = 0; i < k; i++) {
+        a += n;
+    }
+    return a
 };
 
 // Do not edit below this line
