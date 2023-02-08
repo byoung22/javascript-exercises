ba85101aa6226b8e82ef424b68a3ddaac564e0e2
Bailey <baileywyoung@gmail.com>
2023-02-07 22:21:46 -0800

    Problen 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let split = s.split('');
+    split = split.reverse('');
+    split = split.join('')
+    return split;
 };
 
 // Do not edit below this line
