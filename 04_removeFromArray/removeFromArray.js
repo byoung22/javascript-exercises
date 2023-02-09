478b4f45c310505afcb95b8e10f5f848e4f22509
Bailey <baileywyoung@gmail.com>
2023-02-09 15:57:03 -0800

    Problem 4 done with all tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, r1, r2, r3, r4) {
+    removeOneNumber(array,r1);
+    removeOneNumber(array,r2);
+    removeOneNumber(array,r3);
+    removeOneNumber(array,r4);
+    return array
 };
 
+function removeOneNumber(array, r) {
+    for (i = 0; i < array.length; i++) {
+        if (r === array[i]) {               // Needs triple = for same type
+            array.splice(i, 1);
+        }
+    }
+    return array
+}
 // Do not edit below this line
 module.exports = removeFromArray;
