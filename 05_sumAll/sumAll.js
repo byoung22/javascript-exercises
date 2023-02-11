1c12c878caa5a32b84144cc45ccdddd922cb77f0
Bailey <baileywyoung@gmail.com>
2023-02-10 17:48:16 -0800

    Problem 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let sum = 0;
+    if(first < last && typeof(first) == typeof(last) && first > 0 && last > 0){
+        for (i = first; i < last + 1; i++) {
+            sum += i;
+        }
+    } else if (last < first && typeof(first) == typeof(last) && first > 0 && last > 0) {
+        for (i = last; i < first + 1; i++) {
+            sum += i;
+        }
+    } else {
+        return 'ERROR'
+    }
+    return sum;
 };
 
 // Do not edit below this line
