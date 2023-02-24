682d73a163dbbc09b71adf6e92bb221198ffcb49
Bailey <baileywyoung@gmail.com>
2023-02-23 19:04:20 -0800

    Completed calculator with all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const summation = array.reduce((total, num) => (total + num), 0)
+  return summation;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const multiplication = array.reduce((total, num) => (total * num), 1)
+  return multiplication;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let result = 1;
+  for (let i = 0; i < power; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) return 1;
+  for (let i = a - 1; i > 1; i--) {
+      a *= i;
+  }
+  return a
 };
 
 // Do not edit below this line
