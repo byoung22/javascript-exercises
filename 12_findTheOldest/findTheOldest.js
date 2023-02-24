3dc63d57281044c9f80dc3e629e8a6e5e1349608
Bailey <baileywyoung@gmail.com>
2023-02-24 14:10:27 -0800

    findTheOldest completed with alive people tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
+    const year = date.getFullYear();
 
+    const sorted = people.sort(function(a, b) {
+        // Uses current year if not dead
+        (a.yearOfDeath === undefined) ? a.yearOfDeath = year : a.yearOfDeath = a.yearOfDeath;
+        (b.yearOfDeath === undefined) ? b.yearOfDeath = year : b.yearOfDeath = b.yearOfDeath;
+        
+        if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)){
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+    return sorted[sorted.length - 1];
 };
 
 // Do not edit below this line
