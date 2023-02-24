44475af662d3d2462b801d898ec51e230ecf6e03
Bailey <baileywyoung@gmail.com>
2023-02-24 01:37:47 -0800

    Palindromes done with all test cases passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // Make caps for case sensitivity
+    word = word.toUpperCase()
+    // Make the word into an array of letters
+    let array = [];
+    for (let i = 0; i < word.length; i++) {
+        array[i] = word.slice(i, i + 1);
+    }
+    // Make the array filter out non letters
+    let newArray = array.filter((letter) => (letter == 'A' || letter == 'B' || letter == 'C' || letter == 'D' || letter == 'E' || letter == 'F' || 
+    letter == 'G' || letter == 'H' || letter == 'I' || letter == 'J' || letter == 'K' || letter == 'L' || letter == 'M' || 
+    letter == 'N' || letter == 'O' || letter == 'P' || letter == 'Q' || letter == 'R' || letter == 'S' || letter == 'T' || 
+    letter == 'U' || letter == 'V' || letter == 'W' || letter == 'X' || letter == 'Y' || letter == 'Z'));
+    // Reverse and check
+    backwardsArray = [...newArray].reverse();
+    newArray = newArray.join('');
+    backwardsArray = backwardsArray.join('')
+    console.log (backwardsArray);
+    console.log (newArray);
+    if (newArray === backwardsArray) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
